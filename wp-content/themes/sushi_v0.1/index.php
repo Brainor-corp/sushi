db957db1372e67a1656fa0823dfc03e0d2ad3661
peresekinss <peresekinss@gmail.com>
2019-01-23 09:06:21 +0300

    Работа над выводом модальных окон

--- wp-content/themes/sushi_v0.1/index.php
+++ wp-content/themes/sushi_v0.1/index.php
@@ -11,9 +11,8 @@ get_header(); // подключаем header.php ?>
             <?php	query_posts('cat=25'); // указываем идентификатор вашей рубрики.
             while (have_posts()) : the_post();?>
 
-
                 <div class="item">
-                    <a href="#" class="saleItem" data-toggle="modal" data-target="#modalTopMain1">
+                    <a href="#" class="saleItem" data-toggle="modal" data-target="#modalTopMainMobile<?php the_ID()?>">
                         <span class="cont">
                             <span class="header"><?php the_title(); ?></span>
                             <span class="description">
@@ -26,73 +25,9 @@ get_header(); // подключаем header.php ?>
                     </a>
                 </div>
 
-
-
-                <div class="modal fade" id="modalTopMain1" tabindex="-1" role="dialog"
-                     aria-labelledby="myLargeModalLabel" aria-hidden="true">
-                    <div class="modal-dialog modal-lg">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h5 class="modal-title" id="modalTopMain1">
-                                    <p><?php the_content(); // вывод текста записи ?></p>
-                                </h5>
-                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                    <span aria-hidden="true" class="text-danger">&times;</span>
-                                </button>
-                            </div>
-                            <div class="modal-body">
-                                <p><?php get_field('') ?></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
             <?php endwhile;
             wp_reset_query();
             ?>
-
-<!--            <div class="item">-->
-<!--                <a href="#" class="saleItem" data-toggle="modal" data-target="#modalTopMain1">-->
-<!--                    <span class="cont">-->
-<!--                        <span class="header">-->
-<!--                            Бесплатная доставка при заказе на сумму свыше 1 000-->
-<!--                            <span class=""><i class="fas fa-ruble-sign"></i></span>-->
-<!--                        </span>-->
-<!--                        <span class="description">-->
-<!--                            <p>В любой район города</p>-->
-<!--                        </span>-->
-<!--                    </span>-->
-<!--                    <span class="img">-->
-<!--                        <img src="--><?php //echo get_template_directory_uri(); ?><!--/img/top-item1.jpg" class="img-responsive" alt="Бесплатная доставка при заказе на сумму свыше 1 000 <span class=&quot;ruble-ico&quot;>Р</span>">-->
-<!--                    </span>-->
-<!--                </a>-->
-<!--            </div>-->
-            <div class="item">
-                <a href="#" class="saleItem" data-toggle="modal" data-target="#modalTopMain2">
-                    <span class="cont">
-                        <span class="header">Покупай больше — плати меньше</span>
-                        <span class="description">
-                            <p>Скидка 10% при покупке 4х и более товаров</p>
-                        </span>
-                    </span>
-                    <span class="img">
-                        <img src="<?php echo get_template_directory_uri(); ?>/img/top-item2.jpg" class="img-responsive" alt="Бесплатная доставка при заказе на сумму свыше 1 000 <span class=&quot;ruble-ico&quot;>Р</span>">
-                    </span>
-                </a>
-            </div>
-            <div class="item">
-                <a href="#" class="saleItem" data-toggle="modal" data-target="#modalTopMain3">
-                    <span class="cont">
-                        <span class="header">Счастливые часы: скидка — 10%</span>
-                        <span class="description">
-                            <p>Скидка при заказе в период с 12 до 16</p>
-                        </span>
-                    </span>
-                    <span class="img">
-                        <img src="<?php echo get_template_directory_uri(); ?>/img/top-item3.jpg" class="img-responsive" alt="Бесплатная доставка при заказе на сумму свыше 1 000 <span class=&quot;ruble-ico&quot;>Р</span>">
-                    </span>
-                </a>
-            </div>
         </div>
     </div>
 </div>
@@ -100,12 +35,14 @@ get_header(); // подключаем header.php ?>
 <section id="sale" class="saleRama">
     <div class="container-fluid d-lg-block d-none">
         <div class="container nav-container">
+
             <div class="row mb-5">
+                <?php	query_posts('cat=25'); // вместо "5" указываем идентификатор вашей рубрики.
+                while (have_posts()) : the_post();?>
                 <div class="col-md-4 col-12">
-                    <?php	query_posts('cat=25'); // вместо "5" указываем идентификатор вашей рубрики.
-                    while (have_posts()) : the_post();?>
+
                         <div class="item">
-                            <a href="#" class="saleItem" data-toggle="modal" data-target="#modalTopMain1">
+                            <a href="#" class="saleItem" data-toggle="modal" data-target="#modalTopMain<?php the_ID()?>">
                                 <span class="cont">
                                     <span class="header"><?php the_title(); ?></span>
                                     <span class="description">
@@ -117,36 +54,11 @@ get_header(); // подключаем header.php ?>
                                 </span>
                             </a>
                         </div>
-                    <?php endwhile;
-                    wp_reset_query();
-                    ?>
+
                 </div>
-                <div class="col-md-4 col-12">
-                    <a href="#" class="saleItem" data-toggle="modal" data-target="#modalTopMain2">
-                        <span class="cont">
-                            <span class="header">Покупай больше — плати меньше</span>
-                            <span class="description">
-                                <p>Скидка 10% при покупке 4х и более товаров</p>
-                            </span>
-                        </span>
-                        <span class="img">
-                            <img src="<?php echo get_template_directory_uri(); ?>/img/top-item2.jpg" class="img-responsive" alt="Бесплатная доставка при заказе на сумму свыше 1 000 <span class=&quot;ruble-ico&quot;>Р</span>">
-                        </span>
-                    </a>
-                </div>
-                <div class="col-md-4 col-12">
-                    <a href="#" class="saleItem" data-toggle="modal" data-target="#modalTopMain3">
-                        <span class="cont">
-                            <span class="header">Счастливые часы: скидка — 10%</span>
-                            <span class="description">
-                                <p>Скидка при заказе в период с 12 до 16</p>
-                            </span>
-                        </span>
-                        <span class="img">
-                            <img src="<?php echo get_template_directory_uri(); ?>/img/top-item3.jpg" class="img-responsive" alt="Бесплатная доставка при заказе на сумму свыше 1 000 <span class=&quot;ruble-ico&quot;>Р</span>">
-                        </span>
-                    </a>
-                </div>
+                <?php endwhile;
+                wp_reset_query();
+                ?>
             </div>
         </div>
     </div>
@@ -292,78 +204,54 @@ get_header(); // подключаем header.php ?>
 
 <!--modals-->
 
-
-<div class="modal fade" id="modalTopMain1" tabindex="-1" role="dialog"
-     aria-labelledby="myLargeModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-lg">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="modalTopMain1">
-                    <?php the_title(); ?>
-                </h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true" class="text-danger">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <?php the_field('text'); ?>
-            </div>
-        </div>
-    </div>
-</div>
-
-
-
-<div class="modal fade" id="modalTopMain2" tabindex="-1" role="dialog"
-     aria-labelledby="myLargeModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-lg">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="modalTopMain2">
-                    Покупай больше — плати меньше
-                </h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true" class="text-danger">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <?php the_field('text'); ?>
+<!--mobile-->
+<?php	query_posts('cat=25'); // указываем идентификатор вашей рубрики.
+while (have_posts()) : the_post();?>
+    <div class="modal fade" id="modalTopMainMobile<?php the_ID()?>" tabindex="-1" role="dialog"
+         aria-labelledby="myLargeModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="modalTopMainMobile<?php the_ID()?>">
+                        <?php the_title(); ?>
+                    </h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true" class="text-danger">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <?php the_field('text'); ?>
+                </div>
             </div>
         </div>
     </div>
-</div>
-
-
-
-
-<div class="modal fade" id="modalTopMain3" tabindex="-1" role="dialog"
-     aria-labelledby="myLargeModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-lg">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="modalTopMain3">
-                    Счастливые часы: скидка — 10%
-                </h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true" class="text-danger">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <?php the_field('text'); ?>
+<!--mobile-->
+<!--desktop-->
+    <div class="modal fade" id="modalTopMain<?php the_ID()?>" tabindex="-1" role="dialog"
+         aria-labelledby="myLargeModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="modalTopMain<?php the_ID()?>">
+                        <?php the_title(); ?>
+                    </h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true" class="text-danger">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <?php the_field('text'); ?>
+                </div>
             </div>
         </div>
     </div>
-</div>
+<!--desktop-->
+<?php endwhile;
+wp_reset_query();
+?>
+
 
 <!--modals-->
-
-
-
-
-
-
-
-
 
 
 
